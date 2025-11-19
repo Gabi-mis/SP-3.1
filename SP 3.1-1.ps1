fdeb6ad8d6a3a75c0bfdb4729f2e0cc507372ed2
Gabi-mis <kx410tklymipene@gmail.com>
2025-11-19 16:30:09 +0000

    Add files via upload

---
+++ SP 3.1-1.ps1
@@ -0,0 +1,46 @@
+﻿# nombresp31-1lis.ps1
+# Menú de eventos del sistema
+#creo una variable para hacer de accion en el menu
+$op = ""
+#bucle que muestra el menu y cada vez que se reinicie se limpiara la terminal
+while ($op -ne "0") {
+   Clear-Host
+    Write-Host "====== MENU ======"
+    Write-Host "1 - Listado eventos del sistema"
+    Write-Host "2 - Errores del último mes"
+    Write-Host "3 - Warnings de aplicaciones esta semana"
+    Write-Host "0 - Salir"
+#pido al usuario que seleccione una de las opciones del menu
+    $op = Read-Host "Selecciona una opción"
+
+#switch que interpreta la eleccion del usuario
+    switch ($op) {
+        "1" { 
+#muestro los 20 ultimos registros de eventos del sistema mediante el comando get-eventlog el cual coge estos registros del visor de eventos luego esta el comando pause que pone una pausa cuando muestra el registro sugerido para continuar dale a enter
+                Write-Host "`n--- Listado de eventos del sistema ---`n"
+                Get-EventLog -LogName System -Newest 20 
+                Pause 
+                }
+
+        "2" { 
+#muestro los 20 primeros resultados de un filtrado de nuevo sacado con el comando get-eventlog que pilla los registros del visor de eventos el filtrado guarda en una variable el mes pasado y coge los registros con la palabra error y comparando las fechas coge desde esa fecha en adelante por ejemplo si estamos a 1 de abril y ejecutamos el codigo pillara los registros de error de 1 de marzo al 1 de abril luego esta el comando pause que pone una pausa cuando muestra el registro sugerido para continuar dale a enter
+              Write-Host "`n--- Errores del sistema en el mes pasado ---`n"
+              $fecha = (Get-Date).AddMonths(-1)
+              Get-EventLog -LogName System | Where-Object { $_.EntryType -eq "Error" -and $_.TimeGenerated -ge $fecha } | Select -First 20
+              Pause 
+              }
+
+        "3" { 
+#este es muy parecido al anterior pero en vez de pillar errores del sistema pilla warnings de aplicaciones y cambiamos el uso de la seleccion de fecha para que pille los warnings de hace 7 dias en adelante tambien lo limita a los 20 primeros registros tambien tiene el pause que ya lo he explicado
+              Write-Host "`n--- Warnings de aplicaciones de esta semana ---`n"
+              $fecha = (Get-Date).AddDays(-7)
+              Get-EventLog -LogName Application | Where-Object { $_.EntryType -eq "Warning" -and $_.TimeGenerated -ge $fecha } | Select -First 20
+              Pause 
+              }
+
+#añado una cuarta opcion que es la salida
+        "0" { Write-Host "Saliendo..." }
+#y añado una ultima que se tomara en caso de que se ponga cualquier otra cosa y dara un error de opcion invalida
+        default { Write-Host "Opción no válida"; Pause }
+    }
+}
---
+++ abiansp31-3ses.ps1
@@ -0,0 +1,30 @@
+﻿# abiansp31-3ses.ps1
+# Uso: .\abiansp31-3ses.ps1 "2025-11-01" "2025-11-10"
+
+#pido las fechas de inicio y fin
+$FechaInicio = Read-Host "Introduce la fecha de inicio (ejemplo: 2025-11-01)"
+$FechaFin    = Read-Host "Introduce la fecha de fin (ejemplo: 2025-11-10)"
+
+Write-Host "Eventos de Inicio de Sesión:`n" -ForegroundColor Cyan
+
+#hacemos un filtrado de logs para solo usar inicos de sesion luego cogemos los logs que estan entre las dos fechas pedidas anteriormente
+Get-WinEvent -FilterHashtable @{LogName='Security'; Id=4624; StartTime=$FechaInicio; EndTime=$FechaFin} |
+#uso un foreach para extraer los valores de usuario y la fecha y hora del inicio y guardarlos en estas dos variables 
+    ForEach-Object {
+        $usuario = $_.Properties[5].Value
+        $hora = $_.TimeCreated
+
+        # descartamos los usuarios creados por sistema como los usuarios de servicios o WIN-7462D23KU4H$ y dejamos solo los que tienen cuentas
+        # asociadas a usuarios reales como el usuario administrador
+        if ($usuario -and
+            $usuario -notmatch '^(SYSTEM|LOCAL SERVICE|NETWORK SERVICE|ANONYMOUS LOGON|DWM.*|UMFD.*|SERVICIO LOCAL|Servicio de red)$' -and
+            $usuario -notmatch '\$$') {
+
+            #ahora se muestra a continuacion la informacion anterior
+            Write-Host ("Fecha: {0} {1} - Usuario: {2}" -f `
+                $hora.ToString("MM/dd/yyyy"), `
+                $hora.ToString("HH:mm:ss"), `
+                $usuario) -ForegroundColor green
+        }
+    }
+
