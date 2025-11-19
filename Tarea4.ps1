f4b7e9b3b8f374ba357c5512a8eec413b2d4af6e
Gabi-mis <kx410tklymipene@gmail.com>
2025-11-19 17:02:03 +0000

    Add files via upload

---
+++ Tarea4.ps1
@@ -0,0 +1,8 @@
+﻿# Obtener fecha y hora actual
+$fechaHora = Get-Date
+
+# Ruta del archivo en el perfil del usuario
+$rutaArchivo = "$env:USERPROFILE\fecha_hora.txt"
+
+# Escribir la fecha y hora en el archivo (crea o sobrescribe)
+$fechaHora | Out-File $rutaArchivo
